ffeec67e829a5e0068c224bbce5d4718a5ea4df3
saikrishna-GCP <70949791+saikrishna-GCP@users.noreply.github.com>
2020-09-09 12:06:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ def hello_world(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Hello world - sai krishna-version 3.0!'
+        return f'Hello world - sai krishna-version 4.0!'
