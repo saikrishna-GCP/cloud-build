ab1362ba9a3c41017793a19cf8f7a7a331bea786
saikrishna-GCP <70949791+saikrishna-GCP@users.noreply.github.com>
2020-09-09 11:52:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 def hello_world(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
     request_json = request.get_json()
     if request.args and 'message' in request.args:
         return request.args.get('message')
